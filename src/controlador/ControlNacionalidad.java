d18be16b1e6f17f01c2dab34cd83a7437113f92c
alexi <alexi@192.168.56.1>
2017-06-03 05:50:49 -0600

    Nacionalidad lista

--- src/controlador/ControlNacionalidad.java
+++ src/controlador/ControlNacionalidad.java
@@ -1,6 +1,13 @@
 package controlador;
 
+import conexion.Conexion;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
+import modelo.Nacionalidad;
 
 /**
  *
@@ -10,22 +17,103 @@ public class ControlNacionalidad implements OperacionesNacionalidad{
 
     @Override
     public String agregarNacionalidad(Object obj) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Conexion con = new Conexion();
+        Connection cn;
+        ResultSet res;
+        Statement st;
+        String sql;
+        String msj=null;
+        Nacionalidad nac = (Nacionalidad) obj;
+        try{
+            Class.forName(con.getDriver());
+            cn=DriverManager.getConnection(con.getUrl(),con.getUser(),con.getClave());
+            st=cn.createStatement();
+            sql="insert into nacionalidad values("+nac.getIdNacionalidad()+",'"+nac.getNombreNacionalidad()+"')";
+            st.executeUpdate(sql);
+            st.close();
+            cn.close();
+            msj="Datos insertados correctamente";
+        }catch(Exception e){
+            msj=e.toString();
+        }
+        return msj;
     }
 
     @Override
     public String modificarNacionalidad(Object obj) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Conexion con = new Conexion();
+        Connection cn;
+        ResultSet res;
+        Statement st;
+        String sql;
+        String msj=null;
+        Nacionalidad nac = (Nacionalidad) obj;
+        try{
+            Class.forName(con.getDriver());
+            cn=DriverManager.getConnection(con.getUrl(),con.getUser(),con.getClave());
+            st=cn.createStatement();
+            sql="update nacionalidad set nombreNacionalidad='"+nac.getNombreNacionalidad()+"' where idNacionalidad="+nac.getIdNacionalidad();
+            st.executeUpdate(sql);
+            st.close();
+            cn.close();
+            msj="Datos actualizados correctamente";
+        }catch(Exception e){
+            msj=e.toString();
+        }
+        return msj;
     }
 
     @Override
     public String eliminarNacionalidad(Object obj) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Conexion con = new Conexion();
+        Connection cn;
+        ResultSet res;
+        Statement st;
+        String sql;
+        String msj=null;
+        Nacionalidad nac = (Nacionalidad) obj;
+        try{
+            Class.forName(con.getDriver());
+            cn=DriverManager.getConnection(con.getUrl(),con.getUser(),con.getClave());
+            st=cn.createStatement();
+            sql="delete from nacionalidad where idNacionalidad="+nac.getIdNacionalidad();
+            st.executeUpdate(sql);
+            st.close();
+            cn.close();
+            msj="Datos eliminados correctamente";
+        }catch(Exception e){
+            msj=e.toString();
+        }
+        return msj;
     }
 
     @Override
     public List mostrarNacionalidad() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Conexion con = new Conexion();
+        Connection cn;
+        ResultSet res;
+        Statement st;
+        String sql;
+        List listanacionalidad = new ArrayList();
+        try{
+            Class.forName(con.getDriver());
+            cn=DriverManager.getConnection(con.getUrl(),con.getUser(),con.getClave());
+            st=cn.createStatement();
+            sql="select * from nacionalidad";
+            res=st.executeQuery(sql);
+            while(res.next()){
+                listanacionalidad.add(new Nacionalidad(
+                res.getInt("idNacionalidad"),
+                res.getString("nombreNacionalidad")
+                ));
+            }
+            res.close();
+            cn.close();
+            st.close();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return listanacionalidad;
     }
     
 }
